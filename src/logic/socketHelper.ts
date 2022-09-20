943400d467c5a44fe7ffcfdd0fb1ba11a105c7e2
MIDLAJ C <mail.midlajc@gmail.com>
2022-09-20 21:32:01 +0530

    code refactoring

--- src/logic/socketHelper.ts
+++ src/logic/socketHelper.ts
@@ -1,9 +1,2 @@
 import { Server as IoServer, Socket } from "socket.io";
-export default {
-  registerRoom: (io: IoServer, roomId: string): void => {
-    console.log("Hello");
-    io.sockets.on("test", (payload) => {
-      console.log("Hello");
-    });
-  },
-};
+export default {};
